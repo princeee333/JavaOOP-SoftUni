033daae73c5ce2931411821bfc1807e3d95a478a
Andrey Nikolaev <108325243+princeee333@users.noreply.github.com>
2022-07-19 23:01:50 +0300

    Add files via upload

--- Inheritance/lab/test/Main.java
+++ Inheritance/lab/test/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Dog dog = new Dog();
-        dog.eat();
-        dog.bark();
+        Puppy puppy=new Puppy();
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
     }
 
 }
